99517de970601731d718ad10a8df1aebe0943cf5
Shivam Goyal <sg.goyal0101@gmail.com>
2023-07-17 17:51:03 +0530

    successfully prepared dataset for simulation

--- data_preprocessing_and_visualization.py
+++ data_preprocessing_and_visualization.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import collections
 from pprint import pprint
 from matplotlib import pyplot as plt
 import pickle
@@ -31,9 +30,16 @@ for i in range(len(data)):
     ):
         data.iat[i,10] += pd.Timedelta(11, 'hours')
 
+# shifting all order dates to 1 to 7th April
+for i in range(len(data)):
+    data.iat[i,10] = data.iat[i,10].replace(
+        day=(data.iat[i,10].day % 7)+1,
+        month=4
+    )
 
 date_set = set()
 green_date_set = set()
+purple_date_set = set()
 
 for i in range(len(data)):
     date_set.add(
@@ -44,12 +50,21 @@ for i in range(len(data)):
         )
     )
 
-    green_date_set.add(data.iat[i,10].replace(
-            hour=6, minute=0, second=0))
-    green_date_set.add(data.iat[i,10].replace(
-            hour=12, minute=0, second=0))
-    green_date_set.add(data.iat[i,10].replace(
-            hour=18, minute=0, second=0))
+    # green_date_set.add(data.iat[i,10].replace(
+    #         hour=6, minute=0, second=0))
+    # green_date_set.add(data.iat[i,10].replace(
+    #         hour=12, minute=0, second=0))
+    # green_date_set.add(data.iat[i,10].replace(
+    #         hour=18, minute=0, second=0))
+
+    for j in range(1,24):
+        green_date_set.add(data.iat[i,10].replace(
+            hour=j, minute=0, second=0))
+        
+    for j in [6,12,18]:
+        purple_date_set.add(data.iat[i,10].replace(
+            hour=j, minute=0, second=0))
+
 
 
 fig2 = plt.figure('Mumbai orders Histogram shifted to 8am-10pm slot')
@@ -64,6 +79,8 @@ for k in date_set:
     plt.axvline(x=k, color='#ED4C67', linewidth=1)
 for k in green_date_set:
     plt.axvline(x=k, color='#55A868', linewidth=1)
+for k in purple_date_set:
+    plt.axvline(x=k, color='#8e44ad', linewidth=1)
 
 
 with open('mumbai_orders_histogram_shifted.pkl', 'wb') as file:
@@ -71,11 +88,15 @@ with open('mumbai_orders_histogram_shifted.pkl', 'wb') as file:
 
 
 # ----------------------------------------------------------------------------------------
+
+
+c = Counter()
+
+for i in range(len(data)):
+    c[data.iat[i,10].date()] += 1
+
+pprint(dict(c))
+print(len(dict(c)))
+
+
 plt.show()
-# pprint(date_set)
-# print(len(date_set))
-
-# c = Counter()
-
-# # for i in range(len(data)):
-# #     c[data.iat]
