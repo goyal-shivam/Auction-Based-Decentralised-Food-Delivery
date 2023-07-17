6b63b89eff79509c26bfbdf1b1dea0a943b34a84
Shivam Goyal <sg.goyal0101@gmail.com>
2023-07-17 17:35:06 +0530

    considering only order_place and not any other datetime object

--- data_preprocessing_and_visualization.py
+++ data_preprocessing_and_visualization.py
@@ -3,6 +3,7 @@ import collections
 from pprint import pprint
 from matplotlib import pyplot as plt
 import pickle
+from collections import Counter
 
 data = pd.read_pickle('data/data_preprocessed.pkl')
 
@@ -20,111 +21,36 @@ data = data[
             (data['Delivery_location_longitude'] >= MIN_LONG)
         ]
 
-# '''
-date_set = set()
-green_date_set = set()
+# ------------------------------------------------------------------------------------
 
-for i in range(len(data)):
-    date_set.add(
-        data['order_place'].iloc[i].replace(
-            hour=0,
-            minute=0,
-            second=0
-        )
-    )
-
-    green_date_set.add(data['order_place'].iloc[i].replace(
-            hour=6, minute=0, second=0))
-    green_date_set.add(data['order_place'].iloc[i].replace(
-            hour=12, minute=0, second=0))
-    green_date_set.add(data['order_place'].iloc[i].replace(
-            hour=18, minute=0, second=0))
-
-fig = plt.figure('Histogram showing frequency of orders in Mumbai every 6 hours')
-plt.xlabel("Date (divided in 6hr slots)")
-plt.ylabel("Number of orders")
-
-data['order_place'].hist(
-    bins=sorted(list(date_set.union(green_date_set)))
-)
-
-for k in date_set:
-    plt.axvline(x=k, color='#ED4C67', linewidth=1)
-for k in green_date_set:
-    plt.axvline(x=k, color='#55A868', linewidth=1)
-
-
-with open('mumbai_orders_histogram.pkl', 'wb') as file:
-    pickle.dump(fig,file)
-
-# '''
-
-# ----------------------------------------------------------------------------------------
-count2 = 0
 # shifting all orders to 8am to 10pm time
 for i in range(len(data)):
     if(
-        data.iat[i,9].hour >= 22 or 
-        data.iat[i,9].hour < 8 or
-        data.iat[i,10].hour >= 22 or 
-        data.iat[i,10].hour < 8 or
-        data.iat[i,11].hour >= 22 or 
-        data.iat[i,11].hour < 8
+        data.iat[i,10].hour >= 21 or 
+        data.iat[i,10].hour < 8
     ):
-        data.iat[i,9] += pd.Timedelta(11, 'hours')
         data.iat[i,10] += pd.Timedelta(11, 'hours')
-        data.iat[i,11] += pd.Timedelta(11, 'hours')
-        count2 += 1
-
-count = 0
-
-for i in range(len(data)):
-    if(
-        data.iat[i,9].hour >= 22 or 
-        data.iat[i,9].hour < 8 or
-        data.iat[i,10].hour >= 22 or 
-        data.iat[i,10].hour < 8 or
-        data.iat[i,11].hour >= 22 or 
-        data.iat[i,11].hour < 8
-    ):
-        print(i)
-        print(data.iat[i,9])
-        print(data.iat[i,10])
-        print(data.iat[i,11])
-        print()
-        count += 1
-
-print(f'count of wrong entries = {count}')
-print(f'count of total wrong entries = {count2}')
-pd.options.display.max_columns = None
-pd.options.display.max_rows = None
-# pprint(data[['order_pick', 'order_place', 'order_delivered']])
 
 
-# '''
 date_set = set()
 green_date_set = set()
 
 for i in range(len(data)):
     date_set.add(
-        data['order_place'].iloc[i].replace(
+        data.iat[i,10].replace(
             hour=0,
             minute=0,
             second=0
         )
     )
 
-    # green_date_set.add(data['order_place'].iloc[i].replace(
-    #         hour=6, minute=0, second=0))
-    # green_date_set.add(data['order_place'].iloc[i].replace(
-    #         hour=12, minute=0, second=0))
-    # green_date_set.add(data['order_place'].iloc[i].replace(
-    #         hour=18, minute=0, second=0))
+    green_date_set.add(data.iat[i,10].replace(
+            hour=6, minute=0, second=0))
+    green_date_set.add(data.iat[i,10].replace(
+            hour=12, minute=0, second=0))
+    green_date_set.add(data.iat[i,10].replace(
+            hour=18, minute=0, second=0))
 
-    green_date_set.add(data['order_place'].iloc[i].replace(
-            hour=8, minute=0, second=0))
-    green_date_set.add(data['order_place'].iloc[i].replace(
-            hour=22, minute=0, second=0))
 
 fig2 = plt.figure('Mumbai orders Histogram shifted to 8am-10pm slot')
 plt.xlabel("Date (divided in 6hr slots)")
@@ -144,9 +70,12 @@ with open('mumbai_orders_histogram_shifted.pkl', 'wb') as file:
     pickle.dump(fig2,file)
 
 
-
-
-
-# '''
 # ----------------------------------------------------------------------------------------
 plt.show()
+# pprint(date_set)
+# print(len(date_set))
+
+# c = Counter()
+
+# # for i in range(len(data)):
+# #     c[data.iat]
