4e7bc4b97375f7ab8184cd905fee5ffa393394cd
Shivam Goyal <sg.goyal0101@gmail.com>
2023-07-17 14:30:17 +0530

    midway in coverting all accesses using iat

--- data_preprocessing_and_visualization.py
+++ data_preprocessing_and_visualization.py
@@ -20,6 +20,7 @@ data = data[
             (data['Delivery_location_longitude'] >= MIN_LONG)
         ]
 
+'''
 date_set = set()
 green_date_set = set()
 
@@ -56,4 +57,81 @@ for k in green_date_set:
 with open('mumbai_orders_histogram.pkl', 'wb') as file:
     pickle.dump(fig,file)
 
+'''
+
+# ----------------------------------------------------------------------------------------
+count = 0
+# shifting all orders to 8am to 10pm time
+for i in range(len(data)):
+    if(
+        # data['order_pick'].iloc[i].hour >= 22 or 
+        # data.at[i,'Time_taken(min)'] >= 22 or 
+        data.iat[i,9].hour >= 22 or 
+        # data['order_pick'].iloc[i].hour < 8 or
+        data.iat[i,9].hour < 8 or
+        # data['order_place'].iloc[i].hour >= 22 or 
+        data['order_place'].iloc[i].hour >= 22 or 
+        data['order_place'].iloc[i].hour < 8 or
+        data['order_delivered'].iloc[i].hour >= 22 or 
+        data['order_delivered'].iloc[i].hour < 8
+    ):
+        # data['order_pick'].iloc[i] += pd.Timedelta(10, 'hours')
+        # data.at[i,'order_pick'] += pd.Timedelta(10, 'hours')
+        data.iat[i,9] += pd.Timedelta(10, 'hours')
+        data['order_place'].iloc[i] += pd.Timedelta(10, 'hours')
+        data['order_delivered'].iloc[i] += pd.Timedelta(10, 'hours')
+
+        count += 1
+        if(count >=10):
+            break
+
+
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+# pprint(data[['order_pick', 'order_place', 'order_delivered']])
+
+
+# '''
+date_set = set()
+green_date_set = set()
+
+for i in range(len(data)):
+    date_set.add(
+        data['order_place'].iloc[i].replace(
+            hour=0,
+            minute=0,
+            second=0
+        )
+    )
+
+    green_date_set.add(data['order_place'].iloc[i].replace(
+            hour=6, minute=0, second=0))
+    green_date_set.add(data['order_place'].iloc[i].replace(
+            hour=12, minute=0, second=0))
+    green_date_set.add(data['order_place'].iloc[i].replace(
+            hour=18, minute=0, second=0))
+
+fig2 = plt.figure('Mumbai orders Histogram shifted to 8am-10pm slot')
+plt.xlabel("Date (divided in 6hr slots)")
+plt.ylabel("Number of orders")
+
+data['order_place'].hist(
+    bins=sorted(list(date_set.union(green_date_set)))
+)
+
+for k in date_set:
+    plt.axvline(x=k, color='#ED4C67', linewidth=1)
+for k in green_date_set:
+    plt.axvline(x=k, color='#55A868', linewidth=1)
+
+
+with open('mumbai_orders_histogram_shifted.pkl', 'wb') as file:
+    pickle.dump(fig2,file)
+
+
+
+
+
+# '''
+# ----------------------------------------------------------------------------------------
 plt.show()
