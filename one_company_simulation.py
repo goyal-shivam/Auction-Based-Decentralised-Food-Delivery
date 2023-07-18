f2f91bec98d5eed125f639a1e40b58e343b66ccc
Shivam Goyal <sg.goyal0101@gmail.com>
2023-07-18 16:30:38 +0530

    one_company simulation update

--- one_company_simulation.py
+++ one_company_simulation.py
@@ -21,6 +21,8 @@ LOG_DATA = []
 ORDER_DATA = [] # distance, time
 QUEUE_LENGTH = 0
 
+MAX_LAT, MIN_LAT, MAX_LONG, MIN_LONG = 0,0,0,0
+
 def dist(lat1, long1, lat2, long2):
     # returns the distance between two points by 
     # shortest distance on surface of earth
@@ -28,6 +30,21 @@ def dist(lat1, long1, lat2, long2):
 
 def man_dist(lat1, long1, lat2, long2):
     return dist(lat1, long1, lat2, long1) + dist(lat2, long1, lat2, long2)
+
+def get_index_of_nearest_boy(lat,long,time_now):
+    min_time = 1000000000
+    min_ind = -1
+
+    for i in range(NUM_BOYS):
+        new_time = BOYS[i]['free_at']
+        if(BOYS[i]['free_at'] <= time_now):
+            new_time = time_now
+        new_time += ceil(man_dist(lat, long, BOYS[i]['lat'], BOYS[i]['long'])/BIKE_SPEED)
+        if(min_time > new_time):
+            min_ind = i
+            min_time = new_time
+
+    return min_ind
 
 def save_data(curr_time):
     if(len(LOG_DATA)==0):
@@ -48,10 +65,36 @@ data = pd.read_pickle('data/mumbai_7_days_data.pkl')
 pd.options.display.max_rows = None
 pd.options.display.max_columns = 4
 
+a = 'Restaurant_latitude'
+b = 'Delivery_location_latitude'
+c = 'Restaurant_longitude'
+d = 'Delivery_location_longitude'
+
+MAX_LAT = max(data[a].max(), data[b].max())
+MIN_LAT = min(data[a].min(), data[b].min())
+MAX_LONG = max(data[c].max(), data[d].max())
+MIN_LONG = min(data[c].min(), data[d].min())
+
+
 N = len(data)
 
 
+for i in range(NUM_BOYS):
+    BOYS.append({'lat':randrange(10001), 'long':randrange(10001)})
 
+for i in range(NUM_BOYS):
+    BOYS[i]['lat'] = MIN_LAT + (MAX_LAT - MIN_LAT)*BOYS[i]['lat']/10000
+    BOYS[i]['long'] = MIN_LONG + (MAX_LONG - MIN_LONG)*BOYS[i]['long']/10000
+    BOYS[i]['free_at'] = 0
+
+'''
+if BOYS['free_in'] == 0:
+    then it means that the location of the boy is at it lat, long. else if the free_in has some value, it means that at free_at time in the simulation environment in minutes, the boy will become free, and then it's location will become equal to lat,long which is currently stored in it's entry
+
+to check if boy is free or not, you can simply check whether his free_at is less than or equal to current time of simulation or not. if yes, he is free, else he is busy right now and will become free at his free_at time.
+
+free_at means boy is free at the starting edge of that minute
+'''
 
 ''' data.columns print, min_dist, max_dist, N, freq per day
 0 ID
@@ -80,3 +123,9 @@ max_dist = 66.00012665542367
  datetime.date(2022, 4, 7): 329}
 
 '''
+
+env = simpy.Environment()
+boys = simpy.Resource(env, NUM_BOYS)
+env.process(customer_generator(env, boys))
+
+env.run()
