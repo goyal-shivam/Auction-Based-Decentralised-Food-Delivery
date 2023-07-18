a647f0cb179d6cb7f16cc5e0f768069fbc7ffda5
Shivam Goyal <sg.goyal0101@gmail.com>
2023-07-18 10:39:32 +0530

    rearranging

--- display_graph.py
+++ display_graph.py
@@ -1,7 +1,9 @@
 from matplotlib import pyplot as plt
 import pickle
 
-with open('mumbai_orders_histogram.pkl', 'rb') as file:
-    fig2 = pickle.load(file)
+# with open('data/mumbai_orders_histogram.pkl', 'rb') as file:
+#     fig2 = pickle.load(file)
 
+with open('data/mumbai_orders_histogram_shifted.pkl', 'rb') as file:
+    fig = pickle.load(file)
 plt.show()
